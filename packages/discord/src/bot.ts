39c2acdaf31e1b5dcdc5fc502162971983430b58
Tom Shea <tom@tomshea.dev>
2025-06-14 06:44:48 -0700

    update users on a schedule

--- packages/discord/src/bot.ts
+++ packages/discord/src/bot.ts
@@ -1,7 +1,8 @@
-import { saveUserIfNotExist, setUserAvatar } from "@cyrclebot/data";
 import { Client, Events, GatewayIntentBits } from "discord.js";
 import "dotenv/config";
 import assert from "node:assert";
+import { minute } from "./time";
+import { updateUserList } from "./updateuserlist";
 
 assert(process.env.DISCORD_BOT_TOKEN, "Must specify DISCORD_BOT_TOKEN");
 
@@ -17,23 +18,11 @@ const unreadyClient = new Client({
 unreadyClient.login(process.env.DISCORD_BOT_TOKEN);
 
 await new Promise((resolve) => unreadyClient.once(Events.ClientReady, resolve));
-const client = unreadyClient as Client<true>;
+export const client = unreadyClient as Client<true>;
 console.log(`Ready! Logged in as ${client.user.tag}`);
 
-console.log("updating user list...");
-const guildCollection = await client.guilds.fetch();
-await Promise.all(
-  guildCollection.map(async (oauthGuild) => {
-    const guild = await oauthGuild.fetch();
-    console.log(`fetching members for ${oauthGuild.name}...`);
-    const members = await guild.members.fetch();
-    for (const member of members.values()) {
-      saveUserIfNotExist(member.user.id, member.user.username);
-      const avatar = member.user.avatarURL();
-      if (avatar) {
-        setUserAvatar(member.user.id, avatar);
-      }
-    }
-  })
-);
-console.log("updated user list.");
+await updateUserList();
+
+setInterval(async () => {
+  await updateUserList();
+}, 5 * minute);
